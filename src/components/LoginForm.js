9f7bd070105897908544a932f05083c1e0e52891
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-04-11 15:40:36 +0100

    Create Email Changed action creator

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { Card, CardSection, Input, Button } from "./common";
 
+import actions from "../actions";
+
 export default class LoginForm extends Component {
+  //helper function
+  onEmailChanged(text) {}
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="example@mail.com" />
+          <Input
+            label="Email"
+            placeholder="example@mail.com"
+            onChangeText={this.onEmailChanged.bind(this)}
+          />
         </CardSection>
         <CardSection>
           <Input secureTextEntry label="Password" placeholder="********" />
