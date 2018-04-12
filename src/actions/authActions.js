afa770c28b5c31f942657c4d16063d5c74b6a43c
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-04-12 15:46:52 +0100

    Add authActions for Redux Authentication Actions

---
+++ src/actions/authActions.js
@@ -0,0 +1,61 @@
+import firebase from "firebase";
+
+import { Actions } from "react-native-router-flux";
+
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOADING_ON,
+  LOGIN_USER_FAIL
+} from "./types";
+
+export const emailChanged = text => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = text => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+// NOTE: we must use Redux thunk to handle Async actions creator
+export const loginUser = ({ email, password }) => {
+  return dispatch => {
+    dispatch({ type: LOADING_ON });
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => {
+        //this is called untill the firebase auth is complete
+        loginUserSucces(dispatch, user);
+      })
+      .catch(() => {
+        //sign up the user
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => {
+            //this is called untill the firebase auth is complete
+            loginUserSucces(dispatch, user);
+          })
+          .catch(() => {
+            loginUserFail(dispatch);
+          });
+      });
+  };
+};
+
+const loginUserFail = dispatch => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+const loginUserSucces = (dispatch, user) => {
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: user, error: "" });
+  //this when scene key operate
+  Actions.main();
+};
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import { connect } from "react-redux";
 
-export default class EmployeeCreate extends Component {
+import { Card, CardSection, Input, Button, Spinner } from "./common";
+import { onNameChanged, onPhoneChanged, onSave } from "../actions";
+
+class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee create form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label="Name" placeholder="Tidjini Messaoudi" />
+        </CardSection>
+        <CardSection>
+          <Input label="Phone" placeholder="0541 72 64 72" />
+        </CardSection>
+
+        <CardSection>
+          <Button>Save</Button>
+        </CardSection>
+      </Card>
     );
   }
 }
+
+export default connect(null, { onNameChanged, onPhoneChanged, onSave })(
+  EmployeeCreate
+);
